497768975010a553d81e923b3c51283aa3849d73
foysal <mohibullah2k0@gmail.com>
2024-08-04 17:54:55 +0600

    pages and layouts

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,27 @@
+import Sidebar from "@/components/Sidebar";
+import Image from 'next/image';
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const loggedIn = { firstName: 'Foysal', LastName: 'Ahamed' };
+
+  return (
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn}/>
+
+        <div className="flex size-full flex-col">
+          <div className="root-layout">
+            <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+            <div>
+              mobilenav
+            </div>
+          </div>
+        </div>
+
+        {children}
+    </main>
+  );
+}
